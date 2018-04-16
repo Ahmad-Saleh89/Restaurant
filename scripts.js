47f7b798be88330dc61b6fd0ddcf6dae86395c68
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2018-04-15 20:26:01 -0500

    Slider to About page

--- scripts.js
+++ scripts.js
@@ -42,4 +42,60 @@ $(document).ready(function(){
             }
          }
     });
-})
+
+    // Slider in About Page
+    if (document.location.pathname == "/about/about.html") {
+        var $slider = $('.slider-wrapper').find('.slider');
+        var interval;
+  
+        function startSlider(){
+        interval = setInterval(showUp, 7000);
+        }
+        function stopSlider(){
+        clearInterval(interval);
+        }
+        startSlider();
+    
+        function showUp(){
+        var $currentSlide = $slider.find('.showing');
+        var $nextSlide = $currentSlide.next();
+    
+            if($nextSlide.length){
+                $currentSlide.addClass('disappear');
+                var sliderIndex = $nextSlide.index();
+                var $sliderDot = $('.slider-wrapper').find('.slider-dots span').eq(sliderIndex);
+                $sliderDot.addClass('is-active').prev().removeClass('is-active');
+                setTimeout(function(){
+                $currentSlide.removeClass('showing disappear');
+                $nextSlide.addClass('showing');
+                },1500);
+            }else{
+                // first remove class .is-active from all dots:
+                $('.slider-dots span').removeClass('is-active');
+                // and only add it to the first child:
+                $('.slider-dots span:first').addClass('is-active');
+        
+                $slider.find('li:last').addClass('disappear');
+                setTimeout(function(){
+                $slider.find('li:first').addClass('showing');
+                $slider.find('li:last').removeClass('disappear showing');
+                },1500);
+            }
+        }
+        $('.slider-dots span').on('mouseenter', stopSlider).on('mouseleave', startSlider);
+        // Clickable dots:
+        $('.slider-dots span').click(function(){
+            // first clear all dots from .is-active class:
+            $('.slider-dots span').removeClass('is-active');
+            // then only add it to the clicked dot:
+            $(this).addClass('is-active');
+            // then store the index of the clicked dot:
+            var dotIndex = $('.is-active').index();
+            // then clear all slides from .current class:
+            $('.slider li').removeClass('showing');
+            //  finally add .current to the matched slide:
+            $('.slider li:nth-child('+(dotIndex+1)+')').addClass('showing');
+        });
+
+    }
+});
